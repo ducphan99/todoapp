2dfbdf5f486f40a858338a66e7c49e83d10e1079
Phan Anh Đức <55018920+ducphan99@users.noreply.github.com>
2020-06-08 21:37:10 +0700

    Updates

---
+++ precache-manifest.e7e56bf8128c7eec9b4116b557c9973e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44184a0a4eef56700a53ac4b0435a007",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "1a4f5012c53d40bdd537",
+    "url": "/todoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "26c2b0ed1fe45311444c",
+    "url": "/todoapp/static/js/2.6a4b11d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.6a4b11d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4f5012c53d40bdd537",
+    "url": "/todoapp/static/js/main.7bd4b2f2.chunk.js"
+  },
+  {
+    "revision": "6ca5f068bc6441d15faa",
+    "url": "/todoapp/static/js/runtime-main.ce279410.js"
+  }
+]);
